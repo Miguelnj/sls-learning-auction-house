581c1b23389ebcd5c12d983a4987c775701a839d
MiguelN <miguel.navjor@gmail.com>
2021-09-10 17:33:49 +0100

    Get auctions by status

--- auction-service/src/handlers/getAuctions.js
+++ auction-service/src/handlers/getAuctions.js
@@ -1,16 +1,30 @@
 import AWS from 'aws-sdk';
 import createError from 'http-errors';
 import commonMiddleware from "../lib/commonMiddleware";
+import validator from '@middy/validator';
+import getAuctionsSchema from "../lib/schemas/getAuctionsSchema";
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
     let auctions;
-    
+    console.log(event);
+    let {status} = event.queryStringParameters.status ? event.queryStringParameters : {status: 'OPEN'};
+
+    const params = {
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        IndexName: 'statusAndEndDate',
+        KeyConditionExpression: '#status = :status',
+        ExpressionAttributeValues: {
+            ':status': status.toUpperCase(),
+        },
+        ExpressionAttributeNames: {
+            '#status': 'status'
+        },
+    }
+
     try{
-        const result = await dynamoDB.scan({
-            TableName: process.env.AUCTIONS_TABLE_NAME
-        }).promise();
+        const result = await dynamoDB.query(params).promise();
         auctions = result.Items;
     }catch (error){
         console.error(error);
@@ -23,6 +37,13 @@ async function getAuctions(event, context) {
     };
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions)
+    .use(validator({
+        inputSchema: getAuctionsSchema,
+        ajvOptions: {
+            useDefaults: true,
+            strict: false,
+        }
+    }));
 
 
