91ac68098deea412395f76274912ac2b21242687
MiguelN <miguel.navjor@gmail.com>
2021-09-10 22:48:06 +0100

    Added create auction schema validation

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -2,43 +2,46 @@ import {v4 as uuid} from 'uuid';
 import AWS from 'aws-sdk';
 import createError from 'http-errors';
 import commonMiddleware from "../lib/commonMiddleware";
+import validator from "@middy/validator";
+import createAuctionSchema from "../lib/schemas/createAuctionSchema";
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
 
-  const body = event.body;
-  const now = new Date();
-  const endDate = new Date();
-  endDate.setHours(now.getHours() + 1)
+    const body = event.body;
+    const now = new Date();
+    const endDate = new Date();
+    endDate.setHours(now.getHours() + 1)
 
-  const auction = {
-    id: uuid(),
-    title: body.title,
-    status: 'OPEN',
-    createdAt: now.toISOString(),
-    highestBid: {
-      amount: 0,
-    },
-    endingAt: endDate.toISOString(),
-  };
+    const auction = {
+        id: uuid(),
+        title: body.title,
+        status: 'OPEN',
+        createdAt: now.toISOString(),
+        highestBid: {
+            amount: 0,
+        },
+        endingAt: endDate.toISOString(),
+    };
 
-  try{
-    await dynamoDB.put({
-      TableName: process.env.AUCTIONS_TABLE_NAME,
-      Item: auction
-    }).promise();
-  } catch (error) {
-    console.error(error);
-    throw new createError.InternalServerError(error);
-  }
+    try{
+        await dynamoDB.put({
+            TableName: process.env.AUCTIONS_TABLE_NAME,
+            Item: auction
+        }).promise();
+    } catch (error) {
+        console.error(error);
+        throw new createError.InternalServerError(error);
+    }
 
-  return {
-    statusCode: 201,
-    body: JSON.stringify(auction),
-  };
+    return {
+        statusCode: 201,
+        body: JSON.stringify(auction),
+    };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction)
+    .use(validator({inputSchema: createAuctionSchema}));
 
 
