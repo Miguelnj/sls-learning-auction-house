15466732bc61dcd0256330cc48f929d7d146093d
mnavarro <mnavarro@singularecommerce.com>
2021-09-12 17:44:49 +0100

    Added sending email using the notification service when an auction is finished

--- auction-service/src/lib/closeAuction.js
+++ auction-service/src/lib/closeAuction.js
@@ -1,6 +1,33 @@
 import AWS from 'aws-sdk';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
+
+function notifySeller(title, amount, seller) {
+    const subject = (amount === 0) ? 'The auction for your item has finished' : 'Your item has been sold';
+    const body = (amount === 0) ? `Hello. The auction for "${title}" has finished without any bid on it` :
+        `Hey! your item "${title}" has been sold for $${amount}.`;
+
+    return sqs.sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+            subject: subject,
+            recipient: seller,
+            body: body,
+        }),
+    }).promise();
+}
+
+function notifyBidder(title, amount, bidder) {
+    return sqs.sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+            subject: 'You won an auction',
+            recipient: bidder,
+            body: `What a great deal! You won the bid for a "${title}" for $${amount}`
+        }),
+    }).promise();
+}
 
 export async function closeAuction(auction) {
 
@@ -16,5 +43,16 @@ export async function closeAuction(auction) {
         },
     };
 
-    return await dynamodb.update(params).promise();
+    await dynamodb.update(params).promise();
+
+    const {title, seller, highestBid} = auction;
+    const {amount, bidder} = highestBid;
+
+    const sellerNotificationPromise = notifySeller(title, amount, seller);
+
+    let notificationsPromises = [sellerNotificationPromise];
+    if(amount > 0) notificationsPromises.push(notifyBidder(title, amount, bidder));
+
+    return Promise.all([notificationsPromises]);
+
 }
